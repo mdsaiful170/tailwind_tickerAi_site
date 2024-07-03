65019c87c9d7d8a76367347792f5b7ac88d18b9f
mdsaiful1700 <saifulsylhet00@gmial.com>
2024-07-03 17:28:37 +0600

    total solve

--- js/main.js
+++ js/main.js
@@ -3,40 +3,46 @@
 const html = document.querySelector("html");
 const themeBtn = document.getElementById("theme_btn");
 
-if (localStorage.getItem("mode") == "dark") {
+
+let currentMode = localStorage.getItem("mode");
+
+
+if (currentMode === "dark") {
   darkMode();
 } else {
   lightMode();
 }
-themeBtn.addEventListener("click", (e) => {
-  if (localStorage.getItem("mode") == "light") {
-    darkMode();
-  } else {
+
+
+themeBtn.addEventListener('click', (e) => {
+  if (currentMode === "dark") {
     lightMode();
+    currentMode = "light";
+  } else {
+    darkMode();
+    currentMode = "dark";
   }
-});
+  localStorage.setItem("mode", currentMode);
+})
+
+
 function darkMode() {
   html.classList.add("dark");
   themeBtn.classList.replace("ri-moon-line", "ri-sun-line");
-  localStorage.setItem("mode", "dark");
 }
+
 
 function lightMode() {
   html.classList.remove("dark");
   themeBtn.classList.replace("ri-sun-line", "ri-moon-line");
-  localStorage.setItem("mode", "light");
 }
+
+
 // ~~~~~~~~~~~~~~~~~ scroll up section ~~~~~~~~~~~~~~~~~~~~~~~ //
 
 const scrollUp = () => {
   const scrollUpBtn = document.getElementById("scroll-up");
-  if (window.scrollY >= 100) {
-    scrollUpBtn.classList.add("bottom-5");
-    scrollUpBtn.classList.remove("-bottom-60");
-  } else {
-    scrollUpBtn.classList.add("-bottom-60");
-    scrollUpBtn.classList.remove("bottom-5");
-  }
+ 
 };
 
 const scrollToTop = () => {
